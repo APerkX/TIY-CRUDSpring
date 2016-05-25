80954fd8baf860c00d44cc47f7621098b646cf85
BertrandHustle <scott.greenberg926@gmail.com>
2016-05-25 11:35:32 -0400

    game display works

--- src/main/java/com/theironyard/CrudSpringController.java
+++ src/main/java/com/theironyard/CrudSpringController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class CrudSpringController {
@@ -23,8 +24,14 @@ public class CrudSpringController {
         //redirects to create-game if user is logged in, otherwise redirects to webroot
         if (session.getAttribute("userName") != null){
 
+            //get userName & games list for user
             String userName = session.getAttribute("userName").toString();
+            User user = userRepo.findFirstByName(userName);
+            List<Game> games = gameRepo.findByUser(user);
+
+            //add userName/games list to model
             model.addAttribute("userName", userName);
+            model.addAttribute("games", games);
             return "create-game";
 
         } else {
@@ -35,7 +42,10 @@ public class CrudSpringController {
     @RequestMapping(path = "/login", method = RequestMethod.POST)
     public String login(HttpSession session, String userName, String password, Model model) throws Exception{
 
+        //init user/games
         User user = userRepo.findFirstByName(userName);
+        List<Game> games = gameRepo.findByUser(user);
+
 
         if (user == null){
             user = new User();
@@ -53,7 +63,7 @@ public class CrudSpringController {
         }
 
         session.setAttribute("userName", userName);
-        model.addAttribute("games", gameRepo.findByUser(user));
+        model.addAttribute("games", games);
         return "redirect:/";
 
     }
@@ -65,7 +75,7 @@ public class CrudSpringController {
     }
 
     @RequestMapping(path = "/create-game", method = RequestMethod.POST)
-    public String createGame(HttpSession session, String name, String genre, String platform){
+    public String createGame(HttpSession session, String name, String genre, String platform, Model model){
 
         //kicks to webroot if not logged in
         if (session.getAttribute("userName") == null){
@@ -87,11 +97,17 @@ public class CrudSpringController {
         //saves new game to table
         gameRepo.save(game);
 
+        model.addAttribute("games", gameRepo.findByUser(user));
         return "redirect:/";
 
     }
 
     //todo: make delete-game route
+    @RequestMapping(path = "/delete-game", method = RequestMethod.GET)
+    public String deleteGame(HttpSession session, Model model){
+
+        return "redirect:/";
+    }
 
     @RequestMapping(path = "/edit-game", method = RequestMethod.GET)
     public String editGame(HttpSession session, Model model){
--- src/main/java/com/theironyard/GameRepo.java
+++ src/main/java/com/theironyard/GameRepo.java
@@ -1,13 +1,10 @@
 package com.theironyard;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
 
 public interface GameRepo extends CrudRepository <Game, Integer> {
 
-    @Query("SELECT g FROM Game g, User u WHERE g.user_id = u.id")
     List<Game> findByUser (User user);
 
 }
